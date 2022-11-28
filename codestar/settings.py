848c52a6d1e1972e1a7e4db5d854812788afe2fa
rockroman <rock.rakic@gmail.com>
2022-11-28 11:56:59 +0000

    Finish log in and logout template and get ready to final deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['roman-codestar-blog.herokuapp.com', 'localhost']
 
